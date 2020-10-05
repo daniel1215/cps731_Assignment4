c95b5ea83626ae824840f564b11bebab724a54cc
daniel1215 <36432725+daniel1215@users.noreply.github.com>
2020-10-05 08:38:20 -0400

    Update RecyclerViewAdapter.java
    implement basic view holder and methods

--- RecyclerViewAdapter.java
+++ RecyclerViewAdapter.java
@@ -1,4 +1,40 @@
 package com.example.seongjoon_chung_cps731_500503264_assignment4_redo;
 
-public class RecyclerViewAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView recipeTitle;
+        TextView recipeDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeTitle = itemView.findViewById(R.id.recipeTitle);
+            recipeDescription = itemView.findViewById(R.id.recipeDescription);
+
+        }
+    }
 }

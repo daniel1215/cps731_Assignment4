0b8a5ffb7b8df2afa6b17e8abdf0ffe33ed05d3b
daniel1215 <36432725+daniel1215@users.noreply.github.com>
2020-10-05 09:23:56 -0400

    Update MainActivity.java
    Attach recycler view adapter to the main activity

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.seongjoon_chung_cps731_500503264_assignment4_redo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+
+    private List<String> recipeTitles;
+    private List<String> recipeDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipeTitles = new ArrayList<>();
+        recipeDescription = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerViewAdapter = new RecyclerViewAdapter(recipeTitles, recipeDescription);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerViewAdapter);
+
     }
 }

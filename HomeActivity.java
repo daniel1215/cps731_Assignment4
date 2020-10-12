c21be14af23a7e00c8491f08ecf12a2036531371
daniel1215 <36432725+daniel1215@users.noreply.github.com>
2020-10-12 13:42:53 -0400

    Create HomeActivity.java
    Adding a splash screen to create an opening screen

---
+++ HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.seongjoon_chung_cps731_500503264_assignment4_redo;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 5000;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
